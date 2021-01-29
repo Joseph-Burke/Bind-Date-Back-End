e4e7665e0e52136f9972eccabb349f90fe36a7d9
Joseph Burke <joseph_burke@outlook.com>
2021-01-29 13:45:45 +0000

    Set up an "all-data" route and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def all_data
+    render json: {
+      "houses": House.all.as_json,
+      "users": User.all.as_json,
+      "viewings": Viewing.all.as_json,
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :houses
   resources :viewings
   resources :users
+  get '/', to: 'application#all_data'
 end
