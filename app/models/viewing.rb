65a68de5c51fdd4034dc03a5c069c88334baeda4
Joseph Burke <joseph_burke@outlook.com>
2021-02-03 11:37:34 +0000

    Custom validation: viewing_date_is_in_the_future

--- app/models/viewing.rb
+++ app/models/viewing.rb
@@ -1,4 +1,9 @@
 class Viewing < ApplicationRecord
   belongs_to :house
   belongs_to :user
+  validate :viewing_date_is_in_the_future
+
+  def viewing_date_is_in_the_future
+    errors.add(:when, "can't be in the past") if self.when <= Date.today
+  end
 end
