a09496c82aa15897dfe601db1f25c6715736d32c
Joseph Burke <joseph_burke@outlook.com>
2021-02-03 16:23:57 +0000

    Add .github/workflows/linters.yml, fix offences

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe House, type: :model do
   context 'validation' do
     subject do
-      described_class.new(location: '123 Fake Street', description: 'A lovely, fictional property, perfect for writing model tests.', bedrooms: 4, bathrooms: 2)
+      described_class.new(location: '123 Fake Street',
+                          description: 'A lovely, fictional property, perfect for writing model tests.',
+                          bedrooms: 4,
+                          bathrooms: 2)
     end
 
     it 'is valid when requirements are met' do
@@ -14,17 +17,17 @@ RSpec.describe House, type: :model do
       subject.location = nil
       expect(subject).not_to be_valid
     end
-    
+
     it 'is invalid without a description present' do
       subject.description = nil
       expect(subject).not_to be_valid
     end
-    
+
     it 'is invalid without a bedrooms attribute present' do
       subject.bedrooms = nil
       expect(subject).not_to be_valid
     end
-    
+
     it 'is invalid without a bathrooms attribute present' do
       subject.bathrooms = nil
       expect(subject).not_to be_valid
--- spec/models/viewing_spec.rb
+++ spec/models/viewing_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe Viewing, type: :model do
   context 'validation' do
     subject do
       User.create(name: 'John Smith', email: 'john_smith@testmail.com')
-      House.create(location: '123 Fake Street', description: 'A lovely, fictional property, perfect for writing model tests.', bedrooms: 4, bathrooms: 2)
+      House.create(location: '123 Fake Street',
+                   description: 'A lovely, fictional property, perfect for writing model tests.',
+                   bedrooms: 4,
+                   bathrooms: 2)
       described_class.new(user_id: User.last.id, house_id: House.last.id, when: '20th September 2022 12:00:00')
     end
 
@@ -16,7 +19,7 @@ RSpec.describe Viewing, type: :model do
       subject.user_id = nil
       expect(subject).not_to be_valid
     end
-    
+
     it 'is invalid without a house_id present' do
       subject.house_id = nil
       expect(subject).not_to be_valid
@@ -26,7 +29,7 @@ RSpec.describe Viewing, type: :model do
       subject.when = nil
       expect(subject).not_to be_valid
     end
-    
+
     it 'is invalid if the when attribute is a date in the past' do
       subject.when = '1st January 1900 12:00:00'
       expect(subject).not_to be_valid
